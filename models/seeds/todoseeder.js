c4808dad7fe24cb4943cb6d15f5012cce51c24d0
playcsgo <sam@plum.wine>
2023-02-07 20:22:51 +0800

    Feat: Add todo seeder

---
+++ models/seeds/todoseeder.js
@@ -0,0 +1,25 @@
+console.log('start todoseeder.js')
+const mongoose = require('mongoose')
+const Todo = require('../todo')  // 載入todo model
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoDB error');
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected');
+  for (let i = 0; i < 10; i++) {
+    Todo.create({
+        name: `name: name-${i}`
+      })
+  }
+  console.log('done');
+})
